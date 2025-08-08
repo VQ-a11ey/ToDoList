aefd91121fcfee3a86fa6dd66d46630e6f0b467d
Vicky Qin <vickyqin2010@gmail.com>
2025-08-08 12:54:32 -0700

    Added second view

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -10,15 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("To-Do List")
+                    .font(.title)
+                    .fontWeight(.black)
+                    .padding()
+            Spacer()
+            }
+            .padding()
+            Spacer()
+            Button{
+            } label: {
+                Text("+")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
